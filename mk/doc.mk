bbbb1e1f240494def8394c15b08fdb4e59a7b9f4
Dmitry Ponyatov <dponyatov@gmail.com>
2025-01-11 16:56:40 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- mk/doc.mk
+++ mk/doc.mk
@@ -1,3 +1,7 @@
 .PHONY: doxy
 doxy: .doxygen doc/DoxygenLayout.xml doc/logo.png
 	rm -rf doc/html ; doxygen $< 1>/dev/null
+
+.PHONY: doc
+doc: \
+	$(DOC)/libc/Plauger_The_Standard_C_Library_1991.pdf
