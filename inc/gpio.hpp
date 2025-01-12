7d3d67ccdd27d1afc39eebcab7f7cd2737c00645
Dmitry Ponyatov <dponyatov@gmail.com>
2025-01-13 01:07:31 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- inc/gpio.hpp
+++ inc/gpio.hpp
@@ -1,16 +1,37 @@
 #pragma once
 
+#include <cstdint>
+
 /// @defgroup gpio gpio
 /// @ingroup hal
 /// @brief GPIO: raw pin access & configuration
 /// @{
 
+/// @brief @ref Pin logic (CMOS) state
+enum class State : bool {
+    HIGH = true,  ///< logical `1`
+    LOW = false   ///< logicad `0`
+};
+
+/// @brief MCU port
+class Port {};
+
+/// @brief @ref Pin index in port
+// using Index = uint8_t;
+struct Index {
+    uint8_t i : 4;  ///< `0..15` limited to 4 bit
+};
+
+/// @brief MCU/GPIO digital pin
 class Pin {
-    bool state;
+    State state;  ///< current/cached @ref State
+    Port port;    ///< MCU port
+    Index index;  ///< pin index in @ref port
 
    public:
-    bool get();
-    void set(bool hilow);
+    Pin(Port p, State state);
+    State get();
+    void set(State s);
 };
 
 /// @}
