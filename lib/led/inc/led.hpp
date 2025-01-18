8babfdccbb1154d8e8af2e29317f3a45e5e081c4
Dmitry Ponyatov <dponyatov@gmail.com>
2025-01-18 22:03:21 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- lib/led/inc/led.hpp
+++ lib/led/inc/led.hpp
@@ -4,13 +4,14 @@
 /// @ingroup libhw
 
 #include "gpio.hpp"
+#include <stdint.h>
 
 enum class Color : uint8_t { RED, GREEN, YELLOW, BLUE, WHITE };
 
 class LED {
-    Pin pin;
-    Color color;
+//     Pin pin;
+//     Color color;
 
-   public:
-    LED(Pin pin, Color c);
+//    public:
+//     LED(Pin pin, Color c);
 };
